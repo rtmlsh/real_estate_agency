584615ca7c7e0bfbcd35d42a92742a2d8447e160
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-03-26 15:52:56 +0300

    Add search panel

--- property/admin.py
+++ property/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 from .models import Flat
 
+class SearchFlat(admin.ModelAdmin):
+    search_fields = ['town', 'address', 'owner']
 
-admin.site.register(Flat)
+admin.site.register(Flat, SearchFlat)
