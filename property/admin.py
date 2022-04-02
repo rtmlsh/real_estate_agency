7679bd9b5013a52ff465032f00de02374e9a60ac
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-04-02 18:36:40 +0300

    Add right phone numbers

--- property/admin.py
+++ property/admin.py
@@ -5,10 +5,11 @@ from .models import Flat, Complaint
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
+    list_display = ['address', 'owners_phonenumber', 'owner_pure_phone', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+
 
 class ComplaintAdmin(admin.ModelAdmin):
     raw_id_fields = ['user', 'complaint_flat']
