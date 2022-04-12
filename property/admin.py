76e78021353451d21fa9aff4aa36ba74488a751c
rtmlsh <56894987+rtmlsh@users.noreply.github.com>
2022-04-12 12:08:13 +0300

    Delete unnecessary database attributes

--- property/admin.py
+++ property/admin.py
@@ -12,8 +12,7 @@ class PropertyOwnerOwnFlatsInline(admin.TabularInline):
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ['town', 'address', 'owner']
     readonly_fields = ['created_at']
-    list_display = ['address', 'owners_phonenumber', 'owner_pure_phone',
-                    'price', 'new_building', 'construction_year', 'town']
+    list_display = ['address', 'price', 'new_building', 'construction_year', 'town']
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
